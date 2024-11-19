dcc9646f8a3beff7403d41b6927bc0f8bb28b0a7
HaythamHany95 <haythamhany95@gmail.com>
2024-11-19 15:33:01 +0200

    Done with the app bar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,8 @@
 import { HelloWave } from "@/components/HelloWave";
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import Animated, { FadeInDown } from 'react-native-reanimated'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { router } from "expo-router";
 
 
 export default function HomeScreen() {
@@ -9,27 +11,22 @@ export default function HomeScreen() {
       <View className="bg-blue-600 pt-16 pb-6 px-6">
         <Animated.View className={"flex-row justify-between items-center "} >
           <View>
-            {/* Good Morning with Wave 👋 */}
-            <View>
-              <View className={"flex-row gap-2  items-end"}>
-                <Text className="text-white  text-lg" style={{ fontFamily: 'BarlowMedium' }}  >
-                  Good Morning
-                </Text>
-                <HelloWave />
-              </View>
-
+            <View className={"flex-row gap-2  items-end"}>
+              <Text className="text-white  text-lg" style={{ fontFamily: 'BarlowMedium' }}  >
+                Good Morning
+              </Text>
+              <HelloWave />
             </View>
-
-
-
-
-
-
+            <Text className="text-white text-2xl " style={{ fontFamily: 'BarlowBold' }}>Haytham Hany</Text>
           </View>
-
-
-
+          <MaterialCommunityIcons name="bell-badge-outline" size={27} color="white"></MaterialCommunityIcons>
         </Animated.View>
+        <Pressable onPress={() => { router.push('/explore') }}>
+          <View className="flex-row  items-center mt-4 bg-white/20 rounded-2xl px-4 py-2" >
+            <MaterialCommunityIcons name="magnify" size={27} color="white" />
+            <Text className="text-white mx-2" style={{ fontFamily: 'BarlowMedium' }}>What are you looking for?</Text>
+          </View>
+        </Pressable>
 
       </View>
     </View>
